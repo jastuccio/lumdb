d1a832e0e88dcb71283384c8f1b5be5ba080ff1b
jastuccio <jastuccio@mac.com>
2018-05-13 14:36:20 -0400

    refactor: Change "show/hide" text to an input field - complete lesson 7
    If you need to access a DOM element use a `ref`

--- src/App.js
+++ src/App.js
@@ -3,26 +3,18 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  state = {
-    toggle: false
-  };
-  toggle = () => {
-    this.setState({
-      toggle: !this.state.toggle
-    });
+  submit = () => {
+    console.log(this.text.value);
   };
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text="Welcome to Using Props" toggle={this.state.toggle} />
+          <Welcome text="Welcome to Using Props" />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        {this.state.toggle && <p>This should show and hide</p>}
-        <button onClick={this.toggle}>Show / Hide</button>
+        <input type="text" ref={input => (this.text = input)} />
+        <button onClick={this.submit}>Show Value</button>
       </div>
     );
   }
@@ -30,8 +22,7 @@ class App extends Component {
 
 class Welcome extends Component {
   render() {
-    const { text, toggle } = this.props; //deconstruction
-    console.log(toggle);
+    const { text } = this.props; //deconstruction
     return <h1 className="App-title">{text}</h1>;
   }
 }
