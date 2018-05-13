4713dbdda426af97d042def16e6ebb524f18a3fc
jastuccio <jastuccio@mac.com>
2018-05-13 15:05:19 -0400

    refactor: Add a second input - complete lesson 8
    controlled inputs vs uncontrolled inputs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    input: "Hello"
+  };
+
+  updateInput = event => {
+    this.setState({
+      input: event.target.value.trim()
+    });
+  };
+
   submit = () => {
     console.log(this.text.value);
   };
@@ -13,7 +23,17 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <Welcome text="Welcome to Using Props" />
         </header>
-        <input type="text" ref={input => (this.text = input)} />
+        <h3>{this.state.input}</h3>
+
+        <input // Controlled input
+          type="text"
+          onChange={this.updateInput}
+          value={this.state.input}
+        />
+        <input // Uncontrolled input
+          type="text"
+          ref={input => (this.text = input)}
+        />
         <button onClick={this.submit}>Show Value</button>
       </div>
     );
