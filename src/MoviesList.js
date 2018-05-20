989a376b9114594b84a0684d920f738e21f342a3
jastuccio <jastuccio@mac.com>
2018-05-19 22:53:52 -0400

    refactor: switch routes & route params - complete lesson 16

---
+++ src/MoviesList.js
@@ -0,0 +1,31 @@
+/* eslint react/no-did-mount-set-state: 0 */
+import React, { Component } from 'react';
+import Movie from './Movie';
+
+class MoviesList extends Component {
+  state = {
+    movies: [],
+  };
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        'https://api.themoviedb.org/3/discover/movie?api_key=5c1944d084ae8b1d6e6e205117b75b7b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1',
+      );
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  render() {
+    return (
+      <div>
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+      </div>
+    );
+  }
+}
+
+export default MoviesList;
